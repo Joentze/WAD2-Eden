2648ed5904bc6a12f20d0306e39158e8e055e98d
Joen Tan <joentze01@gmail.com>
2023-10-10 19:50:36 +0800

    feat: added notification transitions

--- src/components/notifications/NotificationStack.vue
+++ src/components/notifications/NotificationStack.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 import { useNotification } from "../../stores/notificationStore";
+import { TransitionRoot } from "@headlessui/vue";
 const notificationStore = useNotification();
 </script>
 <template>
-  <div class="toast" v-if="notificationStore.display">
-    <div v-for="notification in notificationStore.content" class="text-black">
-      {{ notification.title }}
+  <TransitionRoot
+    :show="notificationStore.display"
+    enter="transition-opacity duration-75"
+    enter-from="opacity-0"
+    enter-to="opacity-100"
+    leave="transition-opacity duration-150"
+    leave-from="opacity-100"
+    leave-to="opacity-0"
+  >
+    <div class="toast" v-if="notificationStore.display">
+      <div v-for="notification in notificationStore.content" class="text-black">
+        {{ notification.title }}
+      </div>
     </div>
-  </div>
+  </TransitionRoot>
 </template>
 <script lang="ts"></script>
