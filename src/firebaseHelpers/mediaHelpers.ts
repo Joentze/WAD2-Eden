14ba63621b9ff35334d0e3669be8498070ce016e
Joen Tan <joentze01@gmail.com>
2023-10-16 13:14:54 +0800

    feat: added MediaPostType

--- src/firebaseHelpers/mediaHelpers.ts
+++ src/firebaseHelpers/mediaHelpers.ts
@@ -1,1 +1,16 @@
 //HELPER FUNCTIONS FOR MEDIA DOCUMENTS IN FIREBASE
+import { Timestamp } from "firebase/firestore";
+import { JoinedType } from "./projectHelpers";
+
+type MediaPostType = {
+  postDescription: string;
+  postMentions: JoinedType[];
+  postImages: string[];
+  postTag: string;
+  createdBy: string;
+  creatorId: string;
+  creatorPhotoUrl: string;
+  createdOn: Date | Timestamp;
+};
+
+
