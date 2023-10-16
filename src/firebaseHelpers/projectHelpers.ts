0b65c5b0e228c6d3628fe2148f29b720d4732ba6
Joen Tan <joentze01@gmail.com>
2023-10-16 11:06:29 +0800

    feat: added createNewProject helpers

--- src/firebaseHelpers/projectHelpers.ts
+++ src/firebaseHelpers/projectHelpers.ts
@@ -1,1 +1,28 @@
 // HELPER FUNCTIONS FOR PROJECT DOCUMENTS IN FIREBASE
+
+import { setDoc, collection, doc, Timestamp, getDoc } from "firebase/firestore";
+import { useFirebaseAuth } from "vuefire";
+import { db } from "../firebase";
+
+export type JoinedType = {
+    companyId: string;
+    companyName: string;
+    companyPhotoUrl: string;
+  };
+
+export type ProjectType = {
+    creatorName: string;
+    creatorPhotoUrl: string;
+    joined: JoinedType[];
+    projectStart: Timestamp;
+    projectEnd: Timestamp;
+    projectDescription: string;
+    projectAddress: string;
+    projectSize: number;
+    projectTag: string;
+    completed: boolean;
+  };
+
+export const createNewProject =()=>{
+
+}
