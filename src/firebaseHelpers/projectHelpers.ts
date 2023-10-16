e6af2742e0648b0a6402f2342913794ee3ecc69a
Joen Tan <joentze01@gmail.com>
2023-10-16 11:10:55 +0800

    feat: added firebase function addDoc for creating new projects

--- src/firebaseHelpers/projectHelpers.ts
+++ src/firebaseHelpers/projectHelpers.ts
@@ -1,28 +1,39 @@
 // HELPER FUNCTIONS FOR PROJECT DOCUMENTS IN FIREBASE
 
-import { setDoc, collection, doc, Timestamp, getDoc } from "firebase/firestore";
+import {
+  setDoc,
+  collection,
+  doc,
+  Timestamp,
+  getDoc,
+  addDoc,
+} from "firebase/firestore";
 import { useFirebaseAuth } from "vuefire";
 import { db } from "../firebase";
 
 export type JoinedType = {
-    companyId: string;
-    companyName: string;
-    companyPhotoUrl: string;
-  };
+  companyId: string;
+  companyName: string;
+  companyPhotoUrl: string;
+};
 
 export type ProjectType = {
-    creatorName: string;
-    creatorPhotoUrl: string;
-    joined: JoinedType[];
-    projectStart: Timestamp;
-    projectEnd: Timestamp;
-    projectDescription: string;
-    projectAddress: string;
-    projectSize: number;
-    projectTag: string;
-    completed: boolean;
-  };
+  creatorName: string;
+  creatorPhotoUrl: string;
+  joined: JoinedType[];
+  projectStart: Timestamp;
+  projectEnd: Timestamp;
+  projectDescription: string;
+  projectAddress: string;
+  projectSize: number;
+  projectTag: string;
+  completed: boolean;
+};
 
-export const createNewProject =()=>{
-
-}
+export const createNewProject = async (data: ProjectType): Promise<void> => {
+  try {
+    await addDoc(collection(db, "projects"), data);
+  } catch (e) {
+    throw new Error("Error with creating new project!");
+  }
+};
