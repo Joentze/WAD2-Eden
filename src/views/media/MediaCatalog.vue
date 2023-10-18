3795337ffed07dc82422ae395bd92bcede960b93
Chester Lim <Cheslim16@gmail.com>
2023-10-18 15:51:25 +0800

    feat:mediacat

--- src/views/media/MediaCatalog.vue
+++ src/views/media/MediaCatalog.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="hero min-h-screen bg-base-200">
     <div class="hero-content text-center">
-      <div class="max-w-md">
+      <div class="max-w-2xl">
         <h1 class="text-5xl font-bold text-accent">
           Welcome to our Media Catalog!
         </h1>
