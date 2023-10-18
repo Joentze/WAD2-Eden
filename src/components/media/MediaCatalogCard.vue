289f3c746b8377ecf9b3cfb9a12cabb1f7a058c8
Chester Lim <Cheslim16@gmail.com>
2023-10-18 21:37:38 +0800

    feat: added card

---
+++ src/components/media/MediaCatalogCard.vue
@@ -0,0 +1,41 @@
+<script setup></script>
+<template>
+  <div class="card lg:w-6/12 bg-base-100 shadow-xl justify-self-center">
+    <div class="card-body">
+      <h2 class="card-title text-accent">
+        <div class="avatar">
+          <div class="w-20 rounded-full">
+            <img :src="companyLogo" alt="" />
+          </div>
+        </div>
+        {{ companyName }}
+        <div class="badge badge-secondary">NEW</div>
+      </h2>
+      <h3 class="text-accent">{{ postTitle }}</h3>
+      <p class="text-base-content">
+        {{ postDescription }}
+      </p>
+      <div class="card-actions justify-end">
+        <div class="badge badge-outline">{{ tagOne }}</div>
+        <div class="badge badge-outline">{{ tagTwo }}</div>
+      </div>
+    </div>
+    <figure>
+      <img class="p-1" :src="postMedia" alt="" />
+    </figure>
+  </div>
+  <br>
+</template>
+<script>
+export default {
+  props: {
+    companyName: String,
+    companyLogo: String,
+    postTitle: String,
+    postDescription: String,
+    tagOne: String,
+    tagTwo: String,
+    postMedia: String,
+  },
+};
+</script>
--- src/views/media/MediaCatalog.vue
+++ src/views/media/MediaCatalog.vue
@@ -1,6 +1,11 @@
 <script>
 // import list of activity posts
 // v-for the list and create the below cards accordingly
+import MediaCatalogCard from "../../components/media/MediaCatalogCard.vue";
+
+export default {
+  components: { MediaCatalogCard },
+};
 </script>
 <template>
   <div class="hero min-h-screen bg-base-200">
@@ -15,33 +20,34 @@
       </div>
     </div>
   </div>
-<div class="flex justify-center">
-  <div class="card w-96 bg-base-100 shadow-xl justify-self-center">
-    <div class="card-body">
-      <h2 class="card-title text-accent">
-        <div><img src="" alt=""></div>
-        Company Name Annual Beach Clean-Up
-        <div class="badge badge-secondary">NEW</div>
-      </h2>
-      <p class="text-base-content">
-        Save the world with us! Over the weekend, in collaboration with NEA, we,
-        LeafySG, have organised our annual company wide beach clean-up! We are
-        proud to announce that we have cleared a total of 5kg worth of beach
-        trash!! #LoveSG #SavetheWorld #LeafySG
-      </p>
-      <div class="card-actions justify-end">
-        <div class="badge badge-outline">Climate Action</div>
-        <div class="badge badge-outline">Beach Clean-Up</div>
-      </div>
-    </div>
-    <figure>
-      <img
-        class="p-1"
-        src="https://static.mothership.sg/1/2019/09/Gai-Gai-1.jpg"
-        alt="Beach-clean-up-1"
-      />
-    </figure>
+
+  <div class="flex flex-col justify-center items-center">
+    <MediaCatalogCard 
+    companyName="LeafySG" 
+    companyLogo="https://i.pinimg.com/736x/28/c3/52/28c352e5fc943372be17a199727ae05f.jpg"
+    postTitle="Annual Beach Clean-Up"
+    postDescription=" Save the world with us! Over the weekend, in collaboration with NEA,
+          we, LeafySG, have organised our annual company wide beach clean-up! We
+          are proud to announce that we have cleared a total of 5kg worth of
+          beach trash!! #LoveSG #SavetheWorld #LeafySG"
+    tagOne="Climate Action"
+    tagTwo="Beach Clean-Up"
+    postMedia="https://static.mothership.sg/1/2019/09/Gai-Gai-1.jpg"
+    > 
+    </MediaCatalogCard>
+    <MediaCatalogCard 
+    companyName=" Water4U" 
+    companyLogo="https://st2.depositphotos.com/3487615/5722/v/450/depositphotos_57222633-stock-illustration-waterdrop-fresh-leaf-logo-design.jpg"
+    postTitle="Water Conservation Education Campaign"
+    postDescription="Learn about good water consumption practices at Water4U's education
+          campaign! In a 3 day campaign from 1-3 Oct, Water4U organised a water
+          conservation education campaign for all secondary school students.
+          #Water4U #Waterisprecious"
+    tagOne="Water Conservation"
+    tagTwo="Education"
+    postMedia="https://static1.straitstimes.com.sg/s3fs-public/styles/large30x20/public/articles/2023/03/10/20230308344561551d0489a7-eaed-4472-9cf0-9428d6c03914.jpg?VersionId=ZY0b39Jp3PEBsk7c.fTAtOI1P2mNgUaI"
+    > 
+    
+    </MediaCatalogCard>
   </div>
-</div>
-  
 </template>
